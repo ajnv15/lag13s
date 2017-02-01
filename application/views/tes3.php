83905d1cc4bf345134a528ddd5d43939d35ddf52
Arturo Navarro <aj_nv@hotmail.com>
2017-01-31 18:03:56 -0600

    fix background

--- application/views/tes3.php
+++ application/views/tes3.php
@@ -5,7 +5,7 @@
   <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
   <meta name="generator" content="2017.0.1.363"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script type="text/javascript">
    // Update the 'nojs'/'js' class on the html node
@@ -32,10 +32,21 @@ if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required
     });
     });
 </script>
+<style>
+body{
+background-color: #691F38 !important;
+
+
+}
+</style>
   <title>Inicio</title>
   <!-- CSS -->
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
   <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/site_global.css"/>
   <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/index.css" id="pagesheet"/>
+
    </head>
  <body>
 
