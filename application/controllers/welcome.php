b4cfba482f343768662d33aeafb85a8591f27614
aliciamc <a.marcelenocastillo@gmail.com>
2017-02-09 22:13:22 -0600

    Se agrega post

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -3,15 +3,18 @@
 class Welcome extends CI_Controller {
 
 	public function index(){
-
-
-						  $this->load->view("tes3");
-
-
-
+		$this->load->view('headers');
+		$this->load->model('lag');
+		$this->load->view("tes3");
 
 					}
-
-
+	/*La siguiente función nos permite insertar el nombre y matricula en la tabla de internos*/
+	public function post(){
+	$matricula=$this->input->post("matricula");
+	$nombre=$this->input->post("nombre");
+	$this->load->model('lag');
+	$this->lag->insertar($matricula,$nombre);
+	redirect("welcome/index");
+	}
 
   }
