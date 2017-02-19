a5a313870ac745f189ba7842861fd24219bec987
Arturo Navarro <aj_nv@hotmail.com>
2017-02-19 14:58:05 -0600

    Instrucciones de contacto al telegram

--- application/views/telegram.php
+++ application/views/telegram.php
@@ -1,13 +1,90 @@
+<style>
 
+.overlay {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: black;
+  overflow: hidden;
+  width: 0;
+  height: 100%;
+  transition: .5s ease;
+  opacity: 0.8
+}
 
-      <div class="tab-pane"  id="telegram">
-        <div class="modalfade">
+.container:hover .overlay {
+  width: 100%;
+}
+
+.text {
+  white-space: nowrap;
+  color: white;
+  font-size: 12px;
+  position: absolute;
+  overflow: hidden;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+}
+</style>
+
+      <div class="tab-pane fade"  id="telegram">
+        <div class="modal-content">
+
           <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 col-xl-8 col-lg-offset-2 col-md-offset-2 col-sm-offset-2 col-xs-offset-2 col-xl-offset-2">
             <div class="panel animated fadeInDown">
-              <div class="panel-heading">
-                <h3 class="panel-title">Telegram</h3>
+              <div class="modal-header">
+                <h3 class="modal-title">Telegram</h3>
               </div>
+              <div class="modal-body">
+
+                <div class="row">
+                  <div class="container">
+
+
+                  <div class="col-lg-4">
+                    <img src="<?php echo base_url()."/assets/images/bot_telegram.png";?>" alt="">
+                    <div class="overlay">
+                      <div class="text">Al hacer click sobre nuestro bot se abrirá la ventana de chat</div>
+                  </div>
+                  </div>
+                  <div class="col-lg-3">
+                    <strong>Busca el Bot con el nombre "semana13LAGBot"</strong>
+                  </div>
+                  </div>
+                </div><!--row-->
+
+
+
+                <div class="row">
+                  <div class="container">
+
+
+                  <div class="col-lg-4">
+                    <img src="<?php echo base_url()."/assets/images/telegram_iniciar.png";?>" alt="">
+                    <div class="overlay">
+                      <div class="text">Hello World</div>
+                  </div>
+                  </div>
+                  <div class="col-lg-3">
+                    <strong>Selecciona el bot y da click en la parte inferior en iniciar</strong>
+                  </div>
+                  </div>
+                </div><!--segundo row-->
+
+
+              </div>
+
+                <div class="modal-footer">
+
+                </div>
+
             </div>
           </div>
+
+
+
         </div>
     </div>
