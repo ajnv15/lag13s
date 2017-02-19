ffb27aca15efa6228db3c8fd70d391ded8a0d824
aliciamc <a.marcelenocastillo@gmail.com>
2017-02-18 19:30:14 -0600

    se cambió elementos de iniciar sesión

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -12,7 +12,7 @@ class Welcome extends CI_Controller {
 	/*La siguiente función nos permite insertar el nombre y matricula en la tabla de internos*/
 	public function postInterno(){
 		$matricula=$this->input->post("matricula");
-		$nombre=$this->input->post("nombre");
+		//$nombre=$this->input->post("nombre");
 		$pass=$this->input->post("pass");
 		$this->load->model('lag');
 		$this->lag->insertar($matricula,$nombre,$pass);
@@ -37,13 +37,13 @@ public function iniciarSesion()
 	if($interno){
 		  $this->load->library("session");
 			$this->session->set_userdata('usuario',$interno->nombre);
-			
+
 	}
 	//inicio de sesion y redireccion a nuevo controlador
 	//hacer una funcion que seleccione user y pass de bd para usuarios internos
 	else if($externo){
 
-		$externo=$this->load->model->lag->inicioExterno($nombre,$email,$pass);
+		$externo=$this->load->model->lag->inicioExterno($email,$pass);
 		$query="select * from usuarios_externos";
 		$query2=$this->db->query($query);
 		return $query2->result();
