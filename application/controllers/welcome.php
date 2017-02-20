d3d052f9a917d7ba9c1392474c8dc3e89b4ecd6f
aliciamc <a.marcelenocastillo@gmail.com>
2017-02-19 21:32:57 -0600

    se arregló el registro

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -46,19 +46,19 @@ class Welcome extends CI_Controller {
 		$pass=$this->input->post("pass");
 		$this->load->model('lag');
 		$pass=md5($pass);
-		if(!$this->lag->checarexterno($matricula)){
+		if(!$this->lag->checarexterno($email)){
 				$fd=uniqid("ds");
 				$uniquecode="acx".substr($fd,8,14);
-				if($this->lag->verifUniquecodeExt($uniquecode))
+				if(!$this->lag->verifUniquecodeExt($uniquecode))
 				{
 					$this->lag->insertarexterno($nombre,$email,$pass,$uniquecode);
 				}else{
-						$uniquecode=$unique_code.substr(uniqid(),8,10);
-						$this->lag->insertar($nombre,$email,$pass,$uniquecode);
+						$uniquecode=$uniquecode.substr(uniqid(),8,10);
+						$this->lag->insertarexterno($nombre,$email,$pass,$uniquecode);
 
 					}
 				}
-		redirect("welcome/index");
+		//redirect("welcome/index");
 	}
 
 public function iniciarSesion()
@@ -91,6 +91,11 @@ else {
 
 }
 
+public function registros(){
+			$this->load->view('registroexterno');
+
+
+		}
 
 
 
