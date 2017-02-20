7fb6e23b5d925ab8be4ae36432272b13445d9421
Arturo Navarro <aj_nv@hotmail.com>
2017-02-19 20:49:20 -0600

    vistas para mandar mensaje a todos los usuarios registrados

---
+++ application/views/headwocont.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>LAG</title>
+
+  <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+<style>
+body{
+  background-color: #691f38 !important;
+}
+
+</style>
+
+<body>
+  <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <!--<a class="navbar-brand" href="#"><img src="<?php echo base_url().'assets/images/13_sesion.png';?>" alt=""></a>-->
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+      </ul>
+
+      <!--<ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Cerrar Sesión</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+  </nav>
+
+<div class="tab-content">
---
+++ application/views/msnTele.php
@@ -0,0 +1,32 @@
+<div class="tab-pane active" >
+  <div class="modal-content">
+
+    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 col-xl-8 col-lg-offset-2 col-md-offset-2 col-sm-offset-2 col-xs-offset-2 col-xl-offset-2">
+      <div class="panel animated fadeInDown">
+        <div class="modal-header">
+          <h3 class="modal-title">Telegram</h3>
+        </div>
+        <div class="modal-body">
+
+            <form>
+
+              <div class="form-group">
+                <label for="mensaje"></label>
+                <textarea name="mensaje" rows="8" cols="80" id="mensaje" placeholder="Ingresa el mensaje que se enviara a todos los usuarios"></textarea>
+
+              </div>
+            </form>
+
+        </div>
+
+          <div class="modal-footer">
+
+          </div>
+
+      </div>
+    </div>
+
+
+
+  </div>
+</div>
